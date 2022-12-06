0b1685333b41ee8dc55ace15245e137df6160baf
ErickxBa <105766692+ErickxBa@users.noreply.github.com>
2022-12-06 10:18:04 -0500

    Add files via upload

---
+++ Partido/src/Partido.java
@@ -0,0 +1,15 @@
+public class Partido {
+
+    private Equipo[] equipos;
+    private Balon balon;
+    private Arbitro[] arbitros;
+    private Ubicacion ubicacion;
+    private Marcador marcador;
+
+    private int marcadorInicial;
+    private int marcadorFinal;
+
+    public Partido(int marcadorInicial, int marcadorFinal){
+
+    }
+}
